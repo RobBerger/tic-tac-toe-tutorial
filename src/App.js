6a06e7ffb99d12b2936db6a27edc3626db5aa512
berger <robertberger.dev@gmail.com>
2023-02-21 12:06:33 -0500

    added jumpTo and moves

--- src/App.js
+++ src/App.js
@@ -76,10 +76,28 @@ export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const currentSquares = history[history.length - 1];
 
-function handlePlay(nextSquares) {
-  setHistory([...history, nextSquares]);
-  setXIsNext(!xIsNext);
-}
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove) {
+    // TODO
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
     <div className="game">
