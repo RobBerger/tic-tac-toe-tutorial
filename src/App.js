73b6ba182ae26264fa6a5186e5679cf2996b5710
berger <robertberger.dev@gmail.com>
2023-02-21 11:55:41 -0500

    setup handlePlay

--- src/App.js
+++ src/App.js
@@ -7,10 +7,7 @@ function Square({value, onSquareClick}) {
           </button>
 }
 
-function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
+function Board({ xIsNext, squares, onPlay}) {
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
       return;
@@ -21,8 +18,7 @@ function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -81,7 +77,8 @@ export default function Game() {
   const currentSquares = history[history.length - 1];
 
 function handlePlay(nextSquares) {
-  // TODO
+  setHistory([...history, nextSquares]);
+  setXIsNext(!xIsNext);
 }
 
   return (
